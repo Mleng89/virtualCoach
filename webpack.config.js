48f0147c43f473678605b926a09177ce76ebf6ea
Matthew Leng <mleng89@gmail.com>
2022-06-29 12:53:30 -0400

    EDIT: Fixed webpack config errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 // Instantiate the plugin.
-/* The `template` property defines the source of a template file 
+/* The `template` property defines the source of a template file
 that this plugin will use. We will create it later at the bottom. */
 const htmlPlugin = new HtmlWebPackPlugin({
     template: './src/index.html'
@@ -13,8 +13,8 @@ module.exports = {
     // Our application's entry point.
     entry: './src/index.tsx',
 
-    /*These rules define how to deal with files with given extensions. 
-    For example: .tsx files will be compiled with ts-loader, 
+    /*These rules define how to deal with files with given extensions.
+    For example: .tsx files will be compiled with ts-loader,
     a spcific loader for webpack that knows how to work with TypeScript files.*/
 
     module: {
@@ -37,11 +37,15 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    mode: 'development',
 
     /*plugins needed, some documentation on various plugins webpack has: https://webpack.js.org/plugins/
     ie: new webpack.new webpack.DefinePlugin({Definitions...});*/
     plugins: [htmlPlugin],
-
+    performance: {
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
